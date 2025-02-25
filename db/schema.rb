315c66be0f099bb4984d9179679b805975a65f3b
fersmaia10 <fersmaia@icloud.com>
2025-02-25 00:54:22 -0300

    Criando uma rake task e o arquivo seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_11_025121) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_25_033718) do
   create_table "coins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "descricao"
-    t.string "sigla"
-    t.string "url_imagem"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
