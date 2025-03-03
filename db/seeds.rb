92ade72342379e6b5ee9ac6dedda9c667576d3df
fersmaia10 <fersmaia@icloud.com>
2025-03-03 00:27:51 -0300

    Refatoração do arquivo dev.rake com os spinners e o atributo yield e do arquivo seeds.rb com o atributo find or create by

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,32 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Coin.create!(
-    descricao: "Bitcoin",
-    sigla: "BTC",
-    url_image: "https://assets.chinatechnews.com/wp-content/uploads/bitcoin-logo.jpg"
-)
+spinner = TTY::Spinner.new("[spinner] Cadastrando Moedas...")
+    spinner.auto_spin
+
+coins =
+    [
+        {
+            descricao: "Bitcoin",
+            sigla: "BTC",
+            url_image: "https://assets.chinatechnews.com/wp-content/uploads/bitcoin-logo.jpg"
+        },
+
+        {
+            descricao: "Etherium",
+            sigla: "ETH",
+            url_image: "https://png.pngtree.com/png-vector/20210427/ourmid/pngtree-ethereum-cryptocurrency-coin-icon-png-image_3246438.jpg"
+        },
+
+        {
+            descricao: "Solana",
+            sigla: "SLN",
+            url_image: "https://www.liblogo.com/img-logo/so2809s56c-solana-logo-solana-crypto-logo-png-file-png-all.png "
+        }
+    ]
+
+coins.each do |coin| 
+    Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Concluído!)")
